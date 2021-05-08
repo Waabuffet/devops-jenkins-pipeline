fa3dd7f3f59c3570ff1dbb426fe449cfa494d0d7
Dominique <dominousas@gmail.com>
2021-05-08 16:05:39 +0300

    fixing another test bug "sh -c"

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./.env
 
-docker run -v $JENKINS_WORKSPACE/$JOB_NAME/test:/home/test --name node_test --network $NETWORK_NAME -w /home/test node:latest 'npm install && npm run test-jenkins'
+docker run -v $JENKINS_WORKSPACE/$JOB_NAME/test:/home/test --name node_test --network $NETWORK_NAME -w /home/test node:latest sh -c 'npm install && npm run test-jenkins'
 
 # docker run -v $JENKINS_WORKSPACE/$JOB_NAME/test:/home/test --name node_npm_install --network $NETWORK_NAME -w /home/test node:latest npm run test-jenkins
